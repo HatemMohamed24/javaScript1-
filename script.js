b11f03b39d621803f9d9caf6a829df9c06efeaae
HatemMohamed24 <hm2240257@gmail.com>
2023-11-16 07:22:14 +0300

    session2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hatem") 
+/*console.log("hatem") 
 console.log("Hello World") 
 console.log("Temo")
 console.log("Temo")
@@ -33,3 +33,67 @@ console.log($roma)
 let length = 12 , width = 12 
 console.log("the area is " + length * width) 
 // -------------------- //
+*/
+
+
+
+
+/*let name= prompt("Enter you Name") 
+let age= prompt("Enter your age")
+console.log("My Name is" + name + ",and my age is" + age) 
+*/
+/*
+
+
+
+//  /n  newline 
+let namme = prompt("دخل اسمك ")
+let agee= prompt("دخل سنك") 
+console.log("my name is " + namme + "\n,and my age is " + agee)
+*/
+
+
+
+/*
+
+let n = prompt("enter your name") 
+let a =prompt("enter your age") 
+
+console.log(`My name is ${n} and my age is ${a}`)
+console.log(`My name is ${n} 
+and my age is ${a}`) 
+
+*/
+
+/*
+
+let now = 2023 
+let name = prompt("enter your name")
+let birthDate = prompt("enter your birth date year") 
+
+console.log(`my name is ${name}
+and my age is ${now-birthDate}`) 
+
+*/
+// program take two numbers and output the sum of them 
+
+/*
+let number1 = Number(prompt("ENTER THE FIRST NUMBER"))
+let number2 = Number(prompt("ENTER THE SECOND NUMBER"))
+console.log(`the sum of two numbers = ${number1+number2}`) 
+console.log(typeof number1)
+console.log("111" - 112) //coercion
+console.log ("12" + 12) 
+console.log ("12" * 12)
+console.log ("12"/ 12)
+console.log("13" - "10" + 3 )
+console.log("23" > "100")  // no coercion  first with first ... 
+console.log("123" > 100)
+*/
+
+age = " 10 "
+
+console.log(Number(age)+20) 
+
+h = "10dhsdh"
+console.log(typeof Number(h))
