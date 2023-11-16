8795af5d5926e89d77ca99c6f54cb77eb84ec02f
HatemMohamed24 <hm2240257@gmail.com>
2023-11-16 04:59:35 +0300

    private

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("hatem") 
+console.log("Hello World") 
+console.log("Temo")
+console.log("Temo")
+console.log("Temo")
+console.log("Temo")
+console.log("Temo")
+console.log("Temo")
+
+
+let name = "Hatem"
+console.log(name)
+console.log(name)
+console.log(name)
+console.log(name)
+console.log(name)
+
+//let 123name = "toma"  //false naming to varriable
+//let name = "hosiptal" // hosiptal is place 
+let firstName = "Mohamed" //true varriable 
+console.log(firstName) 
+
+
+//let function = " tyt " // false
+
+let PI = 3.14  // constant 
+
+let $roma = "italy" 
+console.log($roma) 
+
+
+//task in session 
+let length = 12 , width = 12 
+console.log("the area is " + length * width) 
+// -------------------- //
